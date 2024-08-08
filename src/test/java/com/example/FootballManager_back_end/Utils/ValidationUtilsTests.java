546a461ba992ebd9e5b33a2f6a9cc01dee810f38
Nadezhda Todorova <ntodor358@gmail.com>
2024-08-08 13:59:09 +0300

    add tests for validateNumber()

--- src/test/java/com/example/FootballManager_back_end/Utils/ValidationUtilsTests.java
+++ src/test/java/com/example/FootballManager_back_end/Utils/ValidationUtilsTests.java
@@ -1,8 +1,9 @@
 package com.example.FootballManager_back_end.Utils;
 
 import com.example.FootballManager_back_end.Exception.ApiRequestException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationUtilsTests {
     @Test
@@ -12,16 +13,16 @@ class ValidationUtilsTests {
 
     @Test
     void testValidateStringFieldWhenNullValue() {
-        ApiRequestException exception = Assertions.assertThrows(ApiRequestException.class, () ->
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () ->
                 ValidationUtils.validateStringField(null, "First name", 2));
-        Assertions.assertEquals("First name must be at least 2 characters long.", exception.getMessage());
+        assertEquals("First name must be at least 2 characters long.", exception.getMessage());
     }
 
     @Test
     void testValidateStringFieldWhenShortValue() {
-        ApiRequestException exception = Assertions.assertThrows(ApiRequestException.class, () ->
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () ->
                 ValidationUtils.validateStringField("A", "First name", 2));
-        Assertions.assertEquals("First name must be at least 2 characters long.", exception.getMessage());
+        assertEquals("First name must be at least 2 characters long.", exception.getMessage());
     }
 
     @Test
@@ -32,5 +33,56 @@ class ValidationUtilsTests {
     @Test
     void testValidateNumberWhenNoMinOrMax() {
         ValidationUtils.validateNumber(50, "Custom field", null, null);
+    }
+
+    @Test
+    void testValidateNumberWhenBelowMinValue() {
+        int value = 5;
+        String fieldName = "Age";
+        Integer minValue = 10;
+        Integer maxValue = null;
+
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () ->
+                ValidationUtils.validateNumber(value, fieldName, minValue, maxValue)
+        );
+        assertEquals("Age must be at least 10.", exception.getMessage());
+    }
+
+    @Test
+    void testValidateNumberWhenAboveMaxValue() {
+        int value = 150;
+        String fieldName = "Shirt Number";
+        Integer minValue = null;
+        Integer maxValue = 99;
+
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () ->
+                ValidationUtils.validateNumber(value, fieldName, minValue, maxValue)
+        );
+        assertEquals("Shirt Number must be at most 99.", exception.getMessage());
+    }
+
+    @Test
+    void testValidateNumberWhenBelowMinAndAboveMaxValue() {
+        int value = 150;
+        String fieldName = "Speed";
+        Integer minValue = 1;
+        Integer maxValue = 99;
+
+        ApiRequestException exception = assertThrows(ApiRequestException.class, () ->
+                ValidationUtils.validateNumber(value, fieldName, minValue, maxValue)
+        );
+        assertEquals("Speed must be at least 1 and at most 99.", exception.getMessage());
+    }
+
+    @Test
+    void testValidateNumberWhenValidValue() {
+        int value = 50;
+        String fieldName = "Dribble";
+        Integer minValue = 1;
+        Integer maxValue = 99;
+
+        assertDoesNotThrow(() ->
+                ValidationUtils.validateNumber(value, fieldName, minValue, maxValue)
+        );
     }
 }
